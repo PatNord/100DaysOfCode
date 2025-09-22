93c437160ca03f7af3692bcbdb745ae5058f27aa
Patrik Nordlund <Patrik.Nordlund@yahoo.com>
2025-09-22 14:09:27 +0300

    completed with ai. Got weird issues with turtle.

--- day22/main.py
+++ day22/main.py
@@ -1,23 +1,55 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-game_is_on = True
+from scoreboard import Scoreboard
+import time
+
+# Screen setup
 screen = Screen()
-screen.screensize(800,600)
 screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
 
-right_paddle = Paddle()
-right_paddle.create_paddle(350, 0)
-left_paddle = Paddle()
-left_paddle.create_paddle(-350, 0)
+# Game objects
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
-screen.onkey(left_paddle.move_up, "Up")
-screen.onkey(left_paddle.move_down, "Down")
+# Controls
 screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
+# Game loop
+game_is_on = True
 while game_is_on:
-    right_paddle.automated_movement()
+    time.sleep(ball.move_speed)   # control game speed
+    screen.update()
+    ball.move()
 
-                           
+    # Detect collision with top/bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or \
+       (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect right paddle miss
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+        time.sleep(1)
+
+    # Detect left paddle miss
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_position()
+        time.sleep(1)
+
 screen.exitonclick()
--- day22/paddle.py
+++ day22/paddle.py
@@ -1,41 +1,20 @@
 from turtle import Turtle
 
-class Paddle:
-    def __init__(self):
-        self.width = 5
-        self.height = 1
-        self.paddle = None
-        self.up = True
 
-
-    def create_paddle(self, start_x, start_y): #tehdään tämä paremmin vielä
-        self.paddle = Turtle("square")
-        self.paddle.hideturtle()
-        self.paddle.penup()
-        self.paddle.color("white")
-        self.paddle.turtlesize(self.width, self.height)
-        self.paddle.goto(start_x, start_y)
-        self.paddle.showturtle()
+class Paddle(Turtle):
     
-    def move_up(self):
-        if self.paddle:
-            new_y = self.paddle.ycor() + 20 
-            self.paddle.goto(self.paddle.xcor(), new_y)
-    def move_down(self):
-        if self.paddle:
-            new_y = self.paddle.ycor() - 20  
-            self.paddle.goto(self.paddle.xcor(), new_y)
-    
-    def automated_movement(self):
-        if self.up:
-            new_y = self.paddle.ycor() + 10 
-            self.paddle.goto(self.paddle.xcor(), new_y)
-            if self.paddle.ycor() >= 280:
-                self.up = False
-        elif not self.up:
-            new_y = self.paddle.ycor() - 10  
-            self.paddle.goto(self.paddle.xcor(), new_y)
-            if self.paddle.ycor() <= -280:
-                self.up = True
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
